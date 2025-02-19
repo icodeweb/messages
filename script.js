f961616f417c3c337c6e4dd333298dc908089a14
Ibrahim Ismail <ebrahemesmael@gmail.com>
2025-02-19 19:18:59 +0200

    init

---
+++ script.js
@@ -0,0 +1,36 @@
+let messages = [
+    {
+        id: null,
+        type: "text",
+        message: "Hey there 👋"
+    }, {
+        id: null,
+        type: "text",
+        message: "I'm Julian"
+    }, {
+        id: null,
+        type: "text",
+        message: "I design and code things on the web"
+    }, {
+        id: null,
+        type: "text",
+        message: "I'm currently working on anime.js v4"
+    }, {
+        id: null,
+        type: "text",
+        message: "You can find me on X, Bluesky and GitHub"
+    }, {
+        id: null,
+        type: "text",
+        message: "Or contact me directly at hello@julian.gr"
+    }, {
+        id: null,
+        type: "text",
+        message: "Have a nice day"
+    }, {
+        id: null,
+        type: "text",
+        message: "~ J."
+    }
+]
+
